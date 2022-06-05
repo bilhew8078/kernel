d53d03e146de91f8404a5306f7f607c64d02d62f
Bill Hewlett <bill@imagecue.lighting>
2022-06-05 13:43:09 -0700

    yet another attempt at rotary-encoder overlay...

--- copyit.sh
+++ copyit.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+echo
+echo "Enter version # to copy: "
+read version
 echo "********** Copying new OS to Rock Pi E *******"
 echo
 cd /media/bill/DATA/NEV9mini
-scp linux-*-4.4.194-*_arm64.deb rock@192.168.0.242:~/
+ls linux-*-4.4.194-$version-*_arm64.deb
+echo "Press any key to continue..."
+read dummy
+scp linux-*-4.4.194-$version-*_arm64.deb rock@192.168.0.242:~/
 echo "Copy complete...  SSHing into Rock Pi E..."
 ssh rock@192.168.0.242
 
---
+++ installit.sh
@@ -0,0 +1,12 @@
+#1/bin/bash
+
+echo
+echo "************ Debian OS update installer for Rock Pi E ******
+echo
+echo "Enter version number:"
+read version
+sudo dpkg -i linux-image-4.4.194-$version-rockchip-*_arm64.deb linux-firmware-image-4.4.194-$version-rockchip-*_arm64.deb
+echo
+echo "************* COMPLETED.  NOW REBOOT. *********************
+echo
+
