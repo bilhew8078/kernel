4c5b191b8ab42a624e2739c7ef96736bf3ca4b2b
Bill Hewlett <bill@imagecue.lighting>
2022-06-05 15:04:00 -0700

    another fucking try...

--- copyit.sh
+++ copyit.sh
@@ -8,7 +8,9 @@ cd /media/bill/DATA/NEV9mini
 ls linux-*-4.4.194-$version-*_arm64.deb
 echo "Press any key to continue..."
 read dummy
-scp linux-*-4.4.194-$version-*_arm64.deb rock@192.168.0.242:~/
+scp linux-image-4.4.194-$version-rockchip-*_arm64.deb rock@192.168.0.242:~/
+scp linux-firmware-image-4.4.194-$version-rockchip-*_arm64.deb rock@192.168.0.242:~/
+echo
 echo "Copy complete...  SSHing into Rock Pi E..."
 ssh rock@192.168.0.242
 
